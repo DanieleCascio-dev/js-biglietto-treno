d3a8ca496efa132cddecc2e20c815b4db210ad03
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-10-18 16:31:06 +0200

    add: logic to js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,28 @@ const userDistance = parseInt(
 );
 const userAge = parseInt(prompt("Inserisci qui l'età del passeggero"));
 console.log(userAge, userDistance);
+
+// Logica
+
+let price = userDistance * 0.21;
+console.log(price);
+let reduction = 0;
+
+//Sconti?
+
+if (userAge < 18) {
+  reduction = price * 0.2;
+  price = price - reduction;
+  console.log(price);
+} else if (userAge > 65) {
+  reduction = price * 0.4;
+  price = price - reduction;
+  console.log(price);
+} else {
+  price = price;
+  console.log(price);
+}
+
+//Rendo il prezzo con due cifre decimali
+price = price.toFixed(2);
+console.log(price);
